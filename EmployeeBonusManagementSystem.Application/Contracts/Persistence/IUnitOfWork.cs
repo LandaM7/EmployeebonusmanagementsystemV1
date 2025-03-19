d441d6cf2cf971239dd8f0dbe027b1a1eeceeba2
Landa Mindiashvili <lmindiashvili@credo.ge>
2025-03-19 11:10:32 +0400

    UnitOfWork

--- EmployeeBonusManagementSystem.Application/Contracts/Persistence/IUnitOfWork.cs
+++ EmployeeBonusManagementSystem.Application/Contracts/Persistence/IUnitOfWork.cs
@@ -1,4 +1,10 @@
 ﻿namespace EmployeeBonusManagementSystem.Application.Contracts.Persistence;
 public interface IUnitOfWork
 {
+    public interface IUnitOfWork
+    {
+        Task BeginTransactionAsync();
+        Task CommitAsync();
+        Task RollbackAsync();
+    }
 }
---
+++ EmployeeBonusManagementSystem.Persistence/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿namespace EmployeeBonusManagementSystem.Persistence.UnitOfWork;
+
+public class UnitOfWork
+{
+    private readonly ApplicationDbContext _context;
+
+    public UnitOfWork(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task BeginTransactionAsync()
+    {
+        await _context.Database.BeginTransactionAsync();
+    }
+
+    public async Task CommitAsync()
+    {
+        await _context.Database.CommitTransactionAsync();
+    }
+
+    public async Task RollbackAsync()
+    {
+        await _context.Database.RollbackTransactionAsync();
+    }
+}
